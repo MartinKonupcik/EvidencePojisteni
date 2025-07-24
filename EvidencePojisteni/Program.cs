6373c0ba04fa502992d7c7eedc2ce71db244d371
Martin Konupčík <konupcikmartin@gmail.com>
2025-07-24 20:02:42 +0200

    Update user interface and enhance functionality
    Modified `Program.cs` to instantiate `Sprava` and improve UI prompts in Czech. Changed loop condition from '5' to '4' and updated user options.
    
    In `Sprava.cs`, renamed `PridatPojsitene` to `PridatPojistence` and revised its implementation to gather user input for insured person details. Added `VyhledatPojistene` method for searching insured persons by name and surname, with appropriate feedback.

--- EvidencePojisteni/Program.cs
+++ EvidencePojisteni/Program.cs
@@ -1,43 +1,44 @@
 ﻿using EvidencePojisteni;
 
-List<PojisteneOsoby> seznamPojistencu = new List<PojisteneOsoby>();
+var sprava = new Sprava();
+
 Console.WriteLine("---------------------------------");
 Console.WriteLine("Evidence Pojistenych");
 Console.WriteLine("---------------------------------");
-Console.WriteLine();
-Console.WriteLine("Vyberte si akci:");
-var choice = '0';
-while (choice != '5')
+
+char choice = '0';
+while (choice != '4')
 {
     Console.WriteLine();
-    Console.WriteLine("1- Pridat noveho pojisteneho");
-    Console.WriteLine("2- Vypsat vsechny pojistene");
-    Console.WriteLine("3- Vyhledat pojisteneho");
-    Console.WriteLine("4- Konec");
-    
+    Console.WriteLine("Vyberte si akci:");
+    Console.WriteLine("1 - Přidat nového pojištěného");
+    Console.WriteLine("2 - Vypsat všechny pojištěné");
+    Console.WriteLine("3 - Vyhledat pojištěného");
+    Console.WriteLine("4 - Konec");
+
     choice = Console.ReadKey().KeyChar;
     Console.WriteLine();
-    switch
-        (choice)
+
+    switch (choice)
     {
         case '1':
-          
+            sprava.PridatPojistence(); 
             break;
 
         case '2':
-           
+            sprava.VypisPojistene();
             break;
 
         case '3':
-         
+            sprava.VyhledatPojistene();
             break;
 
         case '4':
-            Console.WriteLine("Děkuji za použití aplikace");
+            Console.WriteLine("Děkuji za použití aplikace.");
             break;
 
         default:
-            Console.WriteLine("Invalid choice. Please try again.");
+            Console.WriteLine("Neplatná volba. Zkuste to znovu.");
             break;
     }
 }
