efa16cf4b6f74ada991394ae071041f5b13f4f4a
Martin Konupčík <konupcikmartin@gmail.com>
2025-08-21 22:04:00 +0200

    #
    Add Id property and Type enum to ListItemPolicyDto
    
    This commit introduces a new `Id` property of type `Guid`
    and a `Type` enumeration to the `ListItemPolicyDto` class.
    The `Type` enum includes values for `Life`, `Health`,
    `Property`, `Vehicle`, and `Travel`. Additionally,
    unnecessary `using` directives have been removed for
    code cleanup.

--- EvidencePojisteniDto/ListItemPolicyDto.cs
+++ EvidencePojisteniDto/ListItemPolicyDto.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EvidencePojisteniDto
 {
     public class ListItemPolicyDto
     {
+        public Guid Id { get; set; }
+        public enum Type
+        {
+            Life,
+            Health,
+            Property,
+            Vehicle,
+            Travel
+        }
+        public string Name { get; set; }
     }
 }
