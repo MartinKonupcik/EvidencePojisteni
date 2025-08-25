bed40cfed55ad9b81844095082675c786a6c4083
Martin Konupčík <konupcikmartin@gmail.com>
2025-08-25 10:29:45 +0200

    Add ContractId and PersonId to Policy and ListItemPolicyDto
    Updated the `Policy` class to include `ContractId` and `PersonId` properties. Added constructors for initialization and an `Update` method for modifying `Type` and `Name`. Modified `GetListItem` to return a new `ListItemPolicyDto` with the new properties.
    
    Updated `ListItemPolicyDto` to include `ContractId` and `PersonId` to align with changes in the `Policy` class.

--- EvidencePojisteni.API/Entities/Policy.cs
+++ EvidencePojisteni.API/Entities/Policy.cs
@@ -6,5 +6,27 @@ public class Policy : Entity
 {
     public PolicyType Type { get; set; }
     public string Name { get; set; } = null!;
+    public Guid ContractId { get; set; }
+    public Guid PersonId { get; set; }
+
+public Policy() { }
+    public Policy(Guid id, DetailPolicyDto dto)
+    {
+        Id = id;
+        Type = dto.Type;
+        Name = dto.Name;
+    }
+    public void Update(DetailPolicyDto dto)
+    {
+        Type = dto.Type;
+        Name = dto.Name;
+    }
+    public ListItemPolicyDto GetListItem() => new()
+    {
+        ContractId = ContractId,
+        PersonId = PersonId,
+        Type = Type,
+        Name = Name
+    };
 }
 
--- EvidencePojisteniDto/ListItemPolicyDto.cs
+++ EvidencePojisteniDto/ListItemPolicyDto.cs
@@ -4,7 +4,8 @@ namespace EvidencePojisteniDto
 {
     public class ListItemPolicyDto
     {
-       
+        public Guid ContractId { get; set; }
+        public Guid PersonId { get; set; }
         public PolicyType Type { get; set; }
         public string Name { get; set; }
     }
